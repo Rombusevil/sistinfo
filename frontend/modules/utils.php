6b34bcd58d580587c259bf676399d296ebbca4b1
Iber <iberparodisiri@gmail.com>
2015-11-25 19:48:44 -0300

    Agrego el extends atkNode a la clase utils para que pueda usar el getDB

--- frontend/modules/utils.php
+++ frontend/modules/utils.php
@@ -1,18 +1,23 @@
 <?php
 
-// Devuelve el nextid y lo aumenta en 1
-function incrementSequence($tabla) {
-    $db = &atkGetDb();
+class utils extends atkNode {
 
-    $result = $db->getrows("SELECT nextid from db_sequence where seq_name = '$tabla';");
+    // Devuelve el nextid y lo aumenta en 1
+    function incrementSequence($tabla) {
+        $db = &atkGetDb();
 
-    if ($result == NULL) {
-        $db->query("INSERT INTO db_sequence (seq_name, nextid) VALUES ('$tabla', 2);");
-        $newId = 1;
-    } else {
-        $newId = $result[0]["nextid"] + 1;
-        $db->query("UPDATE db_sequence set nextid =$newId where seq_name = '$tabla';");
+        $result = $db->getrows("SELECT nextid from db_sequence where seq_name = '$tabla';");
+
+        if ($result == NULL) {
+            $db->query("INSERT INTO db_sequence (seq_name, nextid) VALUES ('$tabla', 2);");
+            $newId = 1;
+        } else {
+            $newId = $result[0]["nextid"] + 1;
+            $db->query("UPDATE db_sequence set nextid =$newId where seq_name = '$tabla';");
+        }
+        return $newId;
     }
 
-    return $newId;
 }
+
+
