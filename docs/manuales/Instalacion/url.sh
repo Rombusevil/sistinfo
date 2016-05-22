bf5858ec3e61fc1337d874f65d9191b9baf260f1
Iber <iberparodisiri@gmail.com>
2016-05-22 13:15:52 -0300

    Actualizo el script de obtención de url

--- docs/manuales/Instalacion/url.sh
+++ docs/manuales/Instalacion/url.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-iface=wlan0
+iface=eth0
 ip=$(/sbin/ifconfig | grep -C 1 $iface| grep -v $iface | cut -d' ' -f 10)
-ip=$(echo -e ${ip})
+ip=$(echo -e ${ip})	# Saco espacios innecesarios
 echo "http://"$ip"/pqllana/index.php"
