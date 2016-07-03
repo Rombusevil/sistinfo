b479ac53be8a626ee8da1677dcce63dce9504f86
Iber <iberparodisiri@gmail.com>
2016-07-03 16:36:38 -0300

    Acomodo el script exportador de BD

--- docs/scripts/bkpDb/bbd.php
+++ docs/scripts/bkpDb/bbd.php
@@ -10,12 +10,14 @@
 		while (($line = fgets($varsFile)) !== false) {
 			if(strpos($line,"\$config_db[\"default\"][\"user\"]")!==false){
 				$usr = explode("=",$line);
-				$usr = $usr[1];
+				$usr = explode(";",$usr[1])[0];
+				$usr = trim($usr);
 				$counter ++;
 			}
 			else if (strpos($line,"\$config_db[\"default\"][\"password\"]")!==false){
 				$pwd = explode("=",$line);
-				$pwd = $pwd[1];
+				$pwd = explode(";",$pwd[1])[0];
+				$pwd = trim($pwd);
 				$counter ++;
 			}
 			// Si ya leí usr y pass corto
@@ -28,12 +30,13 @@
 	else return "Error al exportar base de datos";
 	
 
-	$timestamp = date('Y-m-d');
+	$timestamp = date('Y-m-d H-i-s');
 	$name = "pqllana-$timestamp.sql.gz";
 	$baseUrl = $_SERVER['SERVER_NAME'].":".$_SERVER['SERVER_PORT'];
 
-	system("mysqldump -u $usr -p$pwd pqllana | gzip -9 > $name");
-	system("mv $name ..");
+	$cmd = "mysqldump -u ".$usr." -p".$pwd." pqllana | gzip -9 > \"$name\"";
+	system("rm -f pqllana-*.sql.gz");	// Borro backups viejos
+	system($cmd);
 
 	header("Location: http://$baseUrl/$name"); /* Redirect browser */
 	exit();
